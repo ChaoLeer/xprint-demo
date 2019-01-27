a7614307001db5e5c2693073b417a677d79b7048
lichao <lichao@kaike.la>
2019-01-27 16:48:35 +0800

    更新XPrint demo

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import XPrint from 'xprint'
+import 'xprint/dist/vue-print.css'
 Vue.config.productionTip = false
-
+Vue.use(XPrint, {
+  beforePrint() {
+    document.body.style.backgroundColor = '#f00'
+  },
+  afterPrint() {
+    document.body.style.backgroundColor = '#0ff'
+  }
+})
+console.log(XPrint, Vue)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
